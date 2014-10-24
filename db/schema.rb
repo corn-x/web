7e4c900c0c61636a5ce9139976a748959e23ada5
Łukasz Gurdek <git@ukasiu.pl>
2014-10-24 23:17:15 +0200

    added first_name and last_name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024210326) do
+ActiveRecord::Schema.define(version: 20141024211635) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20141024210326) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
