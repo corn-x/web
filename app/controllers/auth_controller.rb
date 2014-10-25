76ed77fa51c847135e798adb5ca11de2f609db67
Michał Furdyna <mr.miskov@gmail.com>
2014-10-25 02:11:50 +0200

    some stuff that is not working yet

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,23 @@
+require 'google/api_client'
+
 class AuthController < ApplicationController
   def add_google_account
-    render json: auth_hash
+    client = Google::APIClient.new(
+        :application_name => 'Example Ruby application',
+        :application_version => '1.0.0'
+    )
+
+    client.authorization.client_id = ENV["GOOGLE_CLIENT_ID"]
+    client.authorization.client_secret = ENV["GOOGLE_CLIENT_SECRET"]
+    client.authorization.scope = %w(https://www.googleapis.com/auth/calendar email profile)
+
+    calendar = client.discovered_api('calendar', 'v3')
+
+    result = client.execute(:api_method => calendar.events.list,
+                                :parameters => {'calendarId' => 'primary'},
+                                :authorization => {code: auth_hash.credentials.token})
+
+    render json: [auth_hash, result.data]
   end
 
   protected
