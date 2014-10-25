eec574573538d962164bee07a2261f6b312cf00c
Michał Furdyna <mr.miskov@gmail.com>
2014-10-25 04:08:39 +0200

    added busy? method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def busy?(start_time, end_time)
+    events = []
+    self.google_calendars.each do |calendar|
+      events += calendar.events.where(
+          '(start_time > ? or end_time < ?) or (start_time < ? and end_time > ?)',
+          start_time, end_time, start_time, end_time
+      )
+    end
+    !events.empty?
+  end
 end
