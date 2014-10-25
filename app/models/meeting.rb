8eed48945ffec63a95216b16a0f70b99f04c469e
Michał Furdyna <mr.miskov@gmail.com>
2014-10-25 15:57:03 +0200

    fixes

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -11,7 +11,7 @@ class Meeting < ActiveRecord::Base
     users = self.team.team_memberships.map { |tm| tm.user }
     slice_times = []
     users.each do |u|
-      slice_times += u.slice_times(self.time_ranges)
+      slice_times << u.slice_times(self.time_ranges)
     end
     slice_times.sort
     previous = slice_times.first
@@ -27,13 +27,13 @@ class Meeting < ActiveRecord::Base
         collisions *= 32
         collisions.to_s(16)
         color = '#' + collisions
-        events += {title: collisions, start: previous, end: time, color: color}
+        events << {title: collisions, start: previous, end: time, color: color}
         previous = time
       end
     end
     if events.empty?
-      time_ranges.each do |time|
-        events += {title: '0', start: time.begin, end: time.end, color: '#00ff00'}
+      time_ranges.map do |time|
+        events << {title: '0', start: time.begin, end: time.end, color: '#00ff00'}
       end
     end
     events
